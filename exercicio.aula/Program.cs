14accda8b3b9d5a24236fe4ea5ac202668385ab0
joao <joaopedro37@gmail.com>
2021-05-05 17:58:27 -0300

    exercicios aula dia 03/05/21

---
+++ exercicio.aula/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace exemplo.aula03._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu nome?");
+            string nome = Console.ReadLine().ToLower();
+
+            string letra = nome.Substring(0, 1);
+
+            switch (letra)
+            {
+                case "a":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Seu Nome começa com a vogal " + letra + " igual Abelha");
+                    break;
+                case "e":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Seu Nome começa com a vogal " + letra + " igual Elefante");
+                    break;
+                case "i":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Seu Nome começa com a vogal " + letra + " igual Igreja ");
+                    break;
+                case "o":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Seu Nome começa com a vogal " + letra + " igual Ovo");
+                    break;
+                case "u":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Seu Nome começa com a vogal " + letra + " igual Urubu");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Seu nome nao começa com uma vogal!");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ exercicio.extra/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace exercicio.extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um numero romano de 1 a 10:");
+            string numero = Console.ReadLine().ToLower();
+            
+            switch (numero)
+            {
+                case "i":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 1");
+                    break;
+                case "ii":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 2");
+                    break;
+                case "iii":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 3");
+                    break;
+                case "iv":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 4");
+                    break;
+                case "v":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 5");
+                    break;
+                case "vi":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 6");
+                    break;
+                case "vii":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 7");
+                    break;
+                case "viii":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 8");
+                    break;
+                case "ix":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 9");
+                    break;
+                case "x":
+                    Console.WriteLine("Em algarismos Indo-Arábicos ele vale: 10");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Este numero nao esta dentro dos valores que foi pedido!!!");
+                    break;
+            }
+            
+        }
+    }
+}
